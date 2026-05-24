4ceafff94a950f9c295be9e9b73bf3fac317c491
tracehillonsign <tracehillonsign@gmail.com>
2026-05-24 13:04:09 +0300

    feat: функция добавления нового контакта

--- include/common.h
+++ include/common.h
@@ -3,3 +3,5 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
+#define EXIT_NO_ARGS 201
--- src/main.c
+++ src/main.c
@@ -1,8 +1,16 @@
 #include "common.h"
+#include "contact.h"
 
 /* Точка входа в программу. */
-int main()
+int main(int argc, char *argv[])
 {
-    printf("Hello, Phone Book!\n");
+    if (argc < 2) {
+        printf("[main:main] Недостаточное количество аргументво.\n");
+        exit(EXIT_NO_ARGS);
+    }
+
+    if (strcmp(argv[1], "add") == 0) {
+        add_contact(argv[2], argv[3]);
+    }
     return 0;
 }
